d51578181ec59a60736789f6383646c185fb0991
AntoineF4C5 <antoinedcht@hotmail.com>
2024-11-23 18:13:21 -0500

    error with verifying signature - bp-ecdsa seems to work only with Secp256k1 yet

--- examples/signing.rs
+++ examples/signing.rs
@@ -2,23 +2,28 @@
 #![allow(non_snake_case)]
 
 use crypto_bigint::{Encoding, U256};
-use ff::PrimeField;
-use halo2curves::group::Group;
-use nova::provider::PallasEngine;
+use ff::{Field, PrimeField};
+use halo2curves::{group::Group, CurveExt};
+use nova::provider::{PallasEngine, VestaEngine};
 use nova::traits::Engine;
 use serde::Serialize;
 use sha2::{Digest, Sha256};
-use std::ops::Mul;
+use std::ops::{Mul, Neg};
 use std::time::Instant;
-use zk_engine::precompiles::signing::{
-  get_points, get_x_affine, get_y_affine, sign, CircuitTypes, SigningCircuit,
-};
+use zk_engine::precompiles::signing::{CircuitTypes, SigningCircuit};
 
-type E1 = nova::provider::PallasEngine;
+use rand::rngs::OsRng;
 
-type Fp = <E1 as Engine>::Base;
-type Fq = <E1 as Engine>::Scalar;
-type GE = <E1 as Engine>::GE;
+type E1 = PallasEngine;
+type E2 = VestaEngine;
+
+type Fp1 = <E1 as Engine>::Base;
+type Fq1 = <E1 as Engine>::Scalar;
+type GE1 = <E1 as Engine>::GE;
+
+type Fp2 = <E2 as Engine>::Base;
+type Fq2 = <E2 as Engine>::Scalar;
+type GE2 = <E2 as Engine>::GE;
 
 #[derive(Serialize)]
 struct SendDataBody {
@@ -44,38 +49,64 @@ fn main() {
   let digest = hasher.finalize();
   let digest: [u8; 32] = digest.as_slice().try_into().unwrap();
 
-  let digest_elem = Fq::from_repr(
+  let digest_elem = Fq1::from_repr(
     U256::from_le_bytes(digest.into())
-      .add_mod(&U256::ZERO, &n)
+      .checked_rem(&n)
+      .unwrap()
       .to_le_bytes(),
   )
   .unwrap();
 
-  let secret_key_u256 = U256::from_le_bytes(secret_key.into());
-  let secret_key_mod = secret_key_u256.checked_rem(&n).unwrap();
-  let secret_key_u256_final = secret_key_mod.to_le_bytes();
+  let secret_key_elem = Fq1::from_repr(
+    U256::from_le_bytes(secret_key.into())
+      .checked_rem(&n)
+      .unwrap()
+      .to_le_bytes(),
+  )
+  .unwrap();
 
-  let secret_key_elem = Fq::from_repr(secret_key_u256_final);
+  // let digest_elem = Fq::from_repr(
+  //   U256::from_le_bytes(digest.into())
+  //     .checked_rem(&n)
+  //     .unwrap()
+  //     .to_le_bytes(),
+  // )
+  // .unwrap();
 
-  if secret_key_elem.is_none().into() {
-    panic!("Invalid secret key");
-  }
+  // let secret_key_elem = Fq::from_repr(
+  //   U256::from_le_bytes(secret_key.into())
+  //     .checked_rem(&n)
+  //     .unwrap()
+  //     .to_le_bytes(),
+  // );
 
-  let secret_key_elem = secret_key_elem.unwrap();
+  // if secret_key_elem.is_none().into() {
+  //   panic!("Invalid secret key");
+  // }
 
-  let g = <PallasEngine as Engine>::GE::generator();
+  // let secret_key_elem = secret_key_elem.unwrap();
 
-  let public_key: <PallasEngine as Engine>::GE = g.mul(secret_key_elem).into();
+  let g = GE1::generator();
+
+  let public_key: GE1 = g.mul(secret_key_elem).into();
 
   let (r, s) = sign(digest_elem, secret_key_elem);
 
   let (t, u) = get_points(r, digest_elem);
 
   let (t_x, t_y) = (get_x_affine(t), get_y_affine(t));
+
+  let lhs = t_y.square();
+  let rhs = t_x.square() * t_x + Fp1::from(5u64);
+  if (lhs != Fp1::ZERO) & (rhs != Fp1::from(5u64)) {
+    // assert only for points other than (0, 0)
+    assert_eq!(lhs, rhs, "(x,y) not on secp256k1");
+  }
+
   let (u_x, u_y) = (get_x_affine(u), get_y_affine(u));
   let (pk_x, pk_y) = (get_x_affine(public_key), get_y_affine(public_key));
 
-  let tx_q = Fq::from_repr(
+  /*   let tx_q = Fq::from_repr(
     U256::from_le_bytes(t_x.to_repr())
       .add_mod(&U256::ZERO, &n)
       .to_le_bytes(),
@@ -110,16 +141,15 @@ fn main() {
       .add_mod(&U256::ZERO, &n)
       .to_le_bytes(),
   )
-  .unwrap();
+  .unwrap(); */
 
   let scalar = U256::from_le_bytes(s.to_repr());
 
-  let circuit_primary: SigningCircuit<Fq> =
-    SigningCircuit::new(scalar, tx_q, ty_q, ux_q, uy_q, pkx_q, pky_q);
+  let circuit_primary: SigningCircuit<Fq2> =
+    SigningCircuit::new(scalar, t_x, t_y, u_x, u_y, pk_x, pk_y);
   // Building circuit's public params
   let start = Instant::now();
-  let pp: nova::PublicParams<nova::provider::PallasEngine> =
-    circuit_primary.get_public_params().unwrap();
+  let pp: nova::PublicParams<VestaEngine> = circuit_primary.get_public_params().unwrap();
   println!("Building public params took {:?}", start.elapsed());
 
   let pp_ser = serde_json::to_string(&pp).unwrap();
@@ -175,4 +205,80 @@ fn main() {
 
   let data_json = serde_json::to_string(&data).unwrap();
   println!("Data to send: {}", data_json);
+}
+
+/// Signs a message using the private key, returns the signature (r, s)
+pub fn sign(msg: Fq1, priv_key: Fq1) -> (GE1, Fq1) {
+  // let mut rng = thread_rng();
+  let n = U256::from_be_hex("40000000000000000000000000000000224698fc0994a8dd8c46eb2100000001");
+  let g = GE1::generator();
+
+  let k = Fq1::random(OsRng);
+  let k_inv = k.invert();
+  assert!(bool::from(k_inv.is_some()));
+  let k_inv = k_inv.unwrap();
+
+  let r: GE1 = g.mul(k).into();
+  let (r_x, _, r_z) = r.jacobian_coordinates();
+  let r_zinv = r_z.invert().unwrap();
+  let r_zinv2 = r_zinv.square();
+  let r_x = r_x * r_zinv2;
+  let r_x = Fq1::from_repr(
+    U256::from_le_bytes(r_x.to_repr())
+      .add_mod(&U256::ZERO, &n)
+      .to_le_bytes()
+      .into(),
+  );
+  assert!(bool::from(r_x.is_some()));
+  let r_x = r_x.unwrap();
+
+  let s = k_inv * (msg + priv_key * r_x);
+
+  (r, s)
+}
+
+/// Gets the T and U points from R (output of message signature) and the message
+pub fn get_points(r: GE1, msg: Fq1) -> (GE1, GE1) {
+  let n = U256::from_be_hex("40000000000000000000000000000000224698fc0994a8dd8c46eb2100000001");
+
+  let g = GE1::generator();
+  let (r_x, _, r_z) = r.jacobian_coordinates();
+  let r_zinv = r_z.invert().unwrap();
+  let r_zinv2 = r_zinv.square();
+  let r_x = r_x * r_zinv2;
+  let r_q = Fq1::from_repr(
+    U256::from_le_bytes(r_x.to_repr())
+      .add_mod(&U256::ZERO, &n)
+      .to_le_bytes()
+      .into(),
+  );
+  assert!(bool::from(r_q.is_some()));
+  let r_q = r_q.unwrap();
+
+  let r_q_inv = r_q.invert();
+  assert!(bool::from(r_q_inv.is_some()));
+  let r_q_inv = r_q_inv.unwrap();
+
+  let t: GE1 = r.mul(r_q_inv).into();
+  let u: GE1 = g.mul(r_q_inv * msg).neg().into();
+
+  (t, u)
+}
+
+/// returns x coordinate of the point in affine coordinates
+pub fn get_x_affine(point: GE1) -> Fp1 {
+  let (x, _, z) = point.jacobian_coordinates();
+  let z_inv = z.invert().unwrap();
+  let z_inv2 = z_inv.square();
+  let x = x * z_inv2;
+  x
+}
+
+/// returns y coordinate of the point in affine coordinates
+pub fn get_y_affine(point: GE1) -> Fp1 {
+  let (_, y, z) = point.jacobian_coordinates();
+  let z_inv = z.invert().unwrap();
+  let z_inv3 = z_inv.square() * z_inv;
+  let y = y * z_inv3;
+  y
 }
--- src/precompiles/signing.rs
+++ src/precompiles/signing.rs
@@ -15,53 +15,51 @@ use nova::{
   },
   CompressedSNARK, PublicParams, RecursiveSNARK,
 };
-use rand::rngs::OsRng;
 use std::error;
-use std::ops::{Mul, Neg};
 
-type Fp = <E1 as Engine>::Base;
-type Fq = <E1 as Engine>::Scalar;
-type Point = <E1 as Engine>::GE;
+type Fq = <E2 as Engine>::Base;
+type Fp = <E2 as Engine>::Scalar;
+type Point = <E2 as Engine>::GE;
 
 /// Create a circuit that signs a message
 /// The default scalar field is PallasEngine's scalar field
 #[derive(Clone)]
-pub struct SigningCircuit<Scalar: PrimeField = Fq> {
+pub struct SigningCircuit<Base: PrimeField = Fp> {
   scalar: U256,
-  t_x: Scalar,
-  t_y: Scalar,
-  u_x: Scalar,
-  u_y: Scalar,
-  public_key_x: Scalar,
-  public_key_y: Scalar,
+  t_x: Base,
+  t_y: Base,
+  u_x: Base,
+  u_y: Base,
+  public_key_x: Base,
+  public_key_y: Base,
 }
 
-impl Default for SigningCircuit<Fq> {
+impl Default for SigningCircuit<Fp> {
   fn default() -> Self {
     Self {
       scalar: U256::ZERO,
-      t_x: Fq::zero(),
-      t_y: Fq::zero(),
-      u_x: Fq::zero(),
-      u_y: Fq::zero(),
-      public_key_x: Fq::zero(),
-      public_key_y: Fq::zero(),
+      t_x: Fp::zero(),
+      t_y: Fp::zero(),
+      u_x: Fp::zero(),
+      u_y: Fp::zero(),
+      public_key_x: Fp::zero(),
+      public_key_y: Fp::zero(),
     }
   }
 }
 
-impl<Scalar: PrimeField + PrimeFieldBits> SigningCircuit<Scalar> {
+impl<Base: PrimeField + PrimeFieldBits> SigningCircuit<Base> {
   /// Create a new signing circuit
   /// - hash: The hash of the message to be signed, as a 32bytes vector
   /// - secret_key: The secret key to sign the message, as a 32bytes vector
   pub fn new(
     scalar: U256,
-    t_x: Scalar,
-    t_y: Scalar,
-    u_x: Scalar,
-    u_y: Scalar,
-    public_key_x: Scalar,
-    public_key_y: Scalar,
+    t_x: Base,
+    t_y: Base,
+    u_x: Base,
+    u_y: Base,
+    public_key_x: Base,
+    public_key_y: Base,
   ) -> Self {
     Self {
       scalar,
@@ -75,8 +73,8 @@ impl<Scalar: PrimeField + PrimeFieldBits> SigningCircuit<Scalar> {
   }
 }
 
-impl<Scalar: PrimeField<Repr = [u8; 32]> + PrimeFieldBits> StepCircuit<Scalar>
-  for SigningCircuit<Scalar>
+impl<Base: PrimeField<Repr = [u8; 32]> + PrimeFieldBits> StepCircuit<Base>
+  for SigningCircuit<Base>
 {
   fn arity(&self) -> usize {
     0
@@ -86,11 +84,11 @@ impl<Scalar: PrimeField<Repr = [u8; 32]> + PrimeFieldBits> StepCircuit<Scalar>
     nova::StepCounterType::Incremental
   }
 
-  fn synthesize<CS: ConstraintSystem<Scalar>>(
+  fn synthesize<CS: ConstraintSystem<Base>>(
     &self,
     cs: &mut CS,
-    _z: &[AllocatedNum<Scalar>],
-  ) -> Result<Vec<AllocatedNum<Scalar>>, SynthesisError> {
+    _z: &[AllocatedNum<Base>],
+  ) -> Result<Vec<AllocatedNum<Base>>, SynthesisError> {
     let t_alloc = AllocatedAffinePoint::alloc_affine_point(cs, self.t_x, self.t_y)?;
 
     let u_alloc = AllocatedAffinePoint::alloc_affine_point(cs, self.u_x, self.u_y)?;
@@ -123,161 +121,87 @@ pub trait CircuitTypes {
   type CompressedProof;
 }
 
-impl CircuitTypes for SigningCircuit<Fq> {
-  type CompressedProof = CompressedSNARK<E1, S1, S2>;
-  type PublicParams = PublicParams<E1>;
+impl CircuitTypes for SigningCircuit<Fp> {
+  type CompressedProof = CompressedSNARK<E2, S2, S1>;
+  type PublicParams = PublicParams<E2>;
 }
 
-impl SigningCircuit<Fq> {
-  // pub fn build_circuit(hash: &[u8], signature: [u8; 65], public_key: <E1 as Engine>::GE);
+/// Builds the public parameters for the circuit
+pub fn get_public_params(
+  circuit: &SigningCircuit<Fp>,
+) -> Result<PublicParams<E2>, Box<dyn error::Error>> {
+  type C2 = TrivialCircuit<<E1 as Engine>::Scalar>;
 
-  /// Builds the public parameters for the circuit
-  pub fn get_public_params(&self) -> Result<PublicParams<E1>, Box<dyn error::Error>> {
-    type C2 = TrivialCircuit<<E2 as Engine>::Scalar>;
+  let circuit_secondary = C2::default();
 
-    let circuit_secondary = C2::default();
+  let pp = PublicParams::<E2>::setup(
+    circuit,
+    &circuit_secondary,
+    &*S2::ck_floor(),
+    &*S1::ck_floor(),
+  )?;
 
-    let pp =
-      PublicParams::<E1>::setup(self, &circuit_secondary, &*S1::ck_floor(), &*S2::ck_floor())?;
-
-    Ok(pp)
-  }
-
-  /// Proves the circuit, this function builds the public params and returns a recursive SNARK
-  pub fn prove(
-    &self,
-    public_params: &PublicParams<E1>,
-  ) -> Result<RecursiveSNARK<E1>, Box<dyn error::Error>> {
-    type C2 = TrivialCircuit<<E2 as Engine>::Scalar>;
-    let circuit_secondary = C2::default();
-    let z0_primary = [<E1 as Engine>::Scalar::ZERO];
-    let z0_secondary = [<E2 as Engine>::Scalar::ZERO];
-
-    let mut recursive_snark: RecursiveSNARK<E1> = RecursiveSNARK::<E1>::new(
-      &public_params,
-      self,
-      &circuit_secondary,
-      &z0_primary,
-      &z0_secondary,
-    )?;
-
-    recursive_snark.prove_step(&public_params, self, &circuit_secondary)?;
-
-    Ok(recursive_snark)
-  }
-
-  /// Compressed a recursive SNARK into a compressed SNARK, using the circuit public params
-  pub fn compress_proof(
-    public_params: &PublicParams<E1>,
-    recursive_snark: &RecursiveSNARK<E1>,
-  ) -> Result<CompressedSNARK<E1, S1, S2>, Box<dyn error::Error>> {
-    let (pk, _) = CompressedSNARK::<E1, S1, S2>::setup(&public_params)?;
-    let compressed_proof =
-      CompressedSNARK::<E1, S1, S2>::prove(&public_params, &pk, recursive_snark)?;
-    Ok(compressed_proof)
-  }
-
-  /// Verifies a recursive SNARK, returns the field elements of the circuit output
-  pub fn verify(
-    public_params: &PublicParams<E1>,
-    recursive_snark: &RecursiveSNARK<E1>,
-  ) -> Result<Vec<<E1 as Engine>::Scalar>, Box<dyn error::Error>> {
-    let z0_primary = [<E1 as Engine>::Scalar::ZERO];
-    let z0_secondary = [<E2 as Engine>::Scalar::ZERO];
-
-    let res = recursive_snark.verify(&public_params, 1, &z0_primary, &z0_secondary);
-    let (vec, _) = res?;
-
-    Ok(vec)
-  }
-
-  /// Verifies a compressed SNARK, returns the field elements of the circuit output
-  pub fn verify_compressed(
-    public_params: &PublicParams<E1>,
-    compressed_proof: &CompressedSNARK<E1, S1, S2>,
-  ) -> Result<Vec<<E1 as Engine>::Scalar>, Box<dyn error::Error>> {
-    let z0_primary = [<E1 as Engine>::Scalar::ZERO];
-    let z0_secondary = [<E2 as Engine>::Scalar::ZERO];
-
-    let (_, vk) = CompressedSNARK::<E1, S1, S2>::setup(&public_params)?;
-    let (res, _) = compressed_proof.verify(&vk, 1, &z0_primary, &z0_secondary)?;
-
-    Ok(res)
-  }
+  Ok(pp)
 }
 
-/// Signs a message using the private key, returns the signature (r, s)
-pub fn sign(msg: Fq, priv_key: Fq) -> (Point, Fq) {
-  // let mut rng = thread_rng();
-  let n = U256::from_be_hex("40000000000000000000000000000000224698fc0994a8dd8c46eb2100000001");
-  let g = Point::generator();
+/// Proves the circuit, this function builds the public params and returns a recursive SNARK
+pub fn prove(
+  circuit: &SigningCircuit<Fp>,
+  public_params: &PublicParams<E2>,
+) -> Result<RecursiveSNARK<E2>, Box<dyn error::Error>> {
+  type C2 = TrivialCircuit<<E1 as Engine>::Scalar>;
+  let circuit_secondary = C2::default();
+  let z0_primary = [<E2 as Engine>::Scalar::ZERO];
+  let z0_secondary = [<E1 as Engine>::Scalar::ZERO];
 
-  let k = Fq::random(OsRng);
-  let k_inv = k.invert();
-  assert!(bool::from(k_inv.is_some()));
-  let k_inv = k_inv.unwrap();
+  let mut recursive_snark: RecursiveSNARK<E2> = RecursiveSNARK::<E2>::new(
+    &public_params,
+    circuit,
+    &circuit_secondary,
+    &z0_primary,
+    &z0_secondary,
+  )?;
 
-  let r: Point = g.mul(k).into();
-  let (r_x, _, r_z) = r.jacobian_coordinates();
-  let r_zinv = r_z.invert().unwrap();
-  let r_zinv2 = r_zinv.square();
-  let r_x = r_x * r_zinv2;
-  let r_x = Fq::from_repr(
-    U256::from_le_bytes(r_x.to_repr())
-      .add_mod(&U256::ZERO, &n)
-      .to_le_bytes()
-      .into(),
-  );
-  assert!(bool::from(r_x.is_some()));
-  let r_x = r_x.unwrap();
+  recursive_snark.prove_step(&public_params, circuit, &circuit_secondary)?;
 
-  let s = k_inv * (msg + priv_key * r_x);
-
-  (r, s)
+  Ok(recursive_snark)
 }
 
-/// returns x coordinate of the point in affine coordinates
-pub fn get_x_affine(point: Point) -> Fp {
-  let (x, _, z) = point.jacobian_coordinates();
-  let z_inv = z.invert().unwrap();
-  let z_inv2 = z_inv.square();
-  let x = x * z_inv2;
-  x
+/// Compressed a recursive SNARK into a compressed SNARK, using the circuit public params
+pub fn compress_proof(
+  public_params: &PublicParams<E2>,
+  recursive_snark: &RecursiveSNARK<E2>,
+) -> Result<CompressedSNARK<E2, S2, S1>, Box<dyn error::Error>> {
+  let (pk, _) = CompressedSNARK::<E2, S2, S1>::setup(&public_params)?;
+  let compressed_proof =
+    CompressedSNARK::<E2, S2, S1>::prove(&public_params, &pk, recursive_snark)?;
+  Ok(compressed_proof)
 }
 
-/// returns y coordinate of the point in affine coordinates
-pub fn get_y_affine(point: Point) -> Fp {
-  let (_, y, z) = point.jacobian_coordinates();
-  let z_inv = z.invert().unwrap();
-  let z_inv3 = z_inv.square() * z_inv;
-  let y = y * z_inv3;
-  y
+/// Verifies a recursive SNARK, returns the field elements of the circuit output
+pub fn verify(
+  public_params: &PublicParams<E2>,
+  recursive_snark: &RecursiveSNARK<E2>,
+) -> Result<Vec<<E2 as Engine>::Scalar>, Box<dyn error::Error>> {
+  let z0_primary = [<E2 as Engine>::Scalar::ZERO];
+  let z0_secondary = [<E1 as Engine>::Scalar::ZERO];
+
+  let res = recursive_snark.verify(&public_params, 1, &z0_primary, &z0_secondary);
+  let (vec, _) = res?;
+
+  Ok(vec)
 }
 
-/// Gets the T and U points from R (output of message signature) and the message
-pub fn get_points(r: Point, msg: Fq) -> (Point, Point) {
-  let n = U256::from_be_hex("40000000000000000000000000000000224698fc0994a8dd8c46eb2100000001");
+/// Verifies a compressed SNARK, returns the field elements of the circuit output
+pub fn verify_compressed(
+  public_params: &PublicParams<E2>,
+  compressed_proof: &CompressedSNARK<E2, S2, S1>,
+) -> Result<Vec<<E2 as Engine>::Scalar>, Box<dyn error::Error>> {
+  let z0_primary = [<E2 as Engine>::Scalar::ZERO];
+  let z0_secondary = [<E1 as Engine>::Scalar::ZERO];
 
-  let g = Point::generator();
-  let (r_x, _, r_z) = r.jacobian_coordinates();
-  let r_zinv = r_z.invert().unwrap();
-  let r_zinv2 = r_zinv.square();
-  let r_x = r_x * r_zinv2;
-  let r_q = Fq::from_repr(
-    U256::from_le_bytes(r_x.to_repr())
-      .add_mod(&U256::ZERO, &n)
-      .to_le_bytes()
-      .into(),
-  );
-  assert!(bool::from(r_q.is_some()));
-  let r_q = r_q.unwrap();
+  let (_, vk) = CompressedSNARK::<E2, S2, S1>::setup(&public_params)?;
+  let (res, _) = compressed_proof.verify(&vk, 1, &z0_primary, &z0_secondary)?;
 
-  let r_q_inv = r_q.invert();
-  assert!(bool::from(r_q_inv.is_some()));
-  let r_q_inv = r_q_inv.unwrap();
-
-  let t: Point = r.mul(r_q_inv).into();
-  let u: Point = g.mul(r_q_inv * msg).neg().into();
-
-  (t, u)
+  Ok(res)
 }
