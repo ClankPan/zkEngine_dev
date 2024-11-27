fd00658bf800c2022781083d3e5cafc11271912b
AntoineF4C5 <antoinedcht@hotmail.com>
2024-11-27 15:35:15 -0500

    IO issue

--- src/precompiles/signing.rs
+++ src/precompiles/signing.rs
@@ -17,16 +17,16 @@ use nova::{
 use std::error;
 
 // Types to be used with circuit's default type parameter
-type E1 = Secp256k1Engine;
-type E2 = Secq256k1Engine;
+type E1 = Secq256k1Engine;
+type E2 = Secp256k1Engine;
 type EE1 = ipa_pc::EvaluationEngine<E1>;
 type EE2 = ipa_pc::EvaluationEngine<E2>;
-type S1 = ppsnark::RelaxedR1CSSNARK<E1, EE1>;
-type S2 = snark::RelaxedR1CSSNARK<E2, EE2>;
+type S1 = snark::RelaxedR1CSSNARK<E1, EE1>;
+type S2 = ppsnark::RelaxedR1CSSNARK<E2, EE2>;
 
 // Working on Secq, so Fq is base field and Fp is scalar field
 // We need Fp as scalar field as signing circuit uses Fp, and building the proof requires the scalar field
-type Fp = <E2 as Engine>::Scalar;
+type Fp = <E1 as Engine>::Scalar;
 
 /// Create a circuit that signs a message
 /// The default scalar field is PallasEngine's scalar field
@@ -70,7 +70,7 @@ impl<Scalar: PrimeField<Repr = [u8; 32]> + PrimeFieldBits> StepCircuit<Scalar>
   for SigningCircuit<Scalar>
 {
   fn arity(&self) -> usize {
-    0
+    1
   }
 
   fn get_counter_type(&self) -> nova::StepCounterType {
@@ -82,19 +82,46 @@ impl<Scalar: PrimeField<Repr = [u8; 32]> + PrimeFieldBits> StepCircuit<Scalar>
     cs: &mut CS,
     _z: &[AllocatedNum<Scalar>],
   ) -> Result<Vec<AllocatedNum<Scalar>>, SynthesisError> {
-    let t_alloc = AllocatedAffinePoint::alloc_affine_point(cs, self.t_x, self.t_y)?;
+    let t_alloc = AllocatedAffinePoint::alloc_affine_point(
+      &mut cs.namespace(|| "alloc t"),
+      self.t_x,
+      self.t_y,
+    )?;
 
-    let u_alloc = AllocatedAffinePoint::alloc_affine_point(cs, self.u_x, self.u_y)?;
+    let u_alloc = AllocatedAffinePoint::alloc_affine_point(
+      &mut cs.namespace(|| "alloc u"),
+      self.u_x,
+      self.u_y,
+    )?;
 
-    let public_key_alloc =
-      AllocatedAffinePoint::alloc_affine_point(cs, self.public_key_x, self.public_key_y)?;
+    let public_key_alloc = AllocatedAffinePoint::alloc_affine_point(
+      &mut cs.namespace(|| "alloc public key"),
+      self.public_key_x,
+      self.public_key_y,
+    )?;
 
-    let out = verify_eff(cs, self.scalar, t_alloc, u_alloc, public_key_alloc).unwrap();
+    let is_valid = verify_eff(
+      &mut cs.namespace(|| "verify signature"),
+      self.scalar,
+      t_alloc,
+      u_alloc,
+      public_key_alloc,
+    )?;
 
-    match Boolean::enforce_equal(cs, &out, &Boolean::Constant(true)) {
-      Ok(_) => Ok(vec![]),
-      Err(e) => Err(e),
-    }
+    let out = is_valid
+      .get_value()
+      .map(|v| if v { Scalar::from(1) } else { Scalar::from(0) })
+      .ok_or(SynthesisError::AssignmentMissing)?;
+
+    let res = AllocatedNum::alloc(&mut cs.namespace(|| "alloc output"), || Ok(out))?;
+
+    Boolean::enforce_equal(
+      &mut cs.namespace(|| "output == true"),
+      &is_valid,
+      &Boolean::Constant(true),
+    )?;
+
+    Ok(vec![res])
   }
 }
 
@@ -107,23 +134,23 @@ pub trait CircuitTypes {
 }
 
 impl CircuitTypes for SigningCircuit<Fp> {
-  type CompressedProof = CompressedSNARK<E2, S2, S1>;
-  type PublicParams = PublicParams<E2>;
+  type CompressedProof = CompressedSNARK<E1, S1, S2>;
+  type PublicParams = PublicParams<E1>;
 }
 
 /// Builds the public parameters for the circuit
 pub fn get_public_params(
   circuit: &SigningCircuit<Fp>,
-) -> Result<PublicParams<E2>, Box<dyn error::Error>> {
-  type C2 = TrivialCircuit<<E1 as Engine>::Scalar>;
+) -> Result<PublicParams<E1>, Box<dyn error::Error>> {
+  type C2 = TrivialCircuit<<E2 as Engine>::Scalar>;
 
   let circuit_secondary = C2::default();
 
-  let pp = PublicParams::<E2>::setup(
+  let pp = PublicParams::<E1>::setup(
     circuit,
     &circuit_secondary,
-    &*S2::ck_floor(),
     &*S1::ck_floor(),
+    &*S2::ck_floor(),
   )?;
 
   Ok(pp)
@@ -132,14 +159,14 @@ pub fn get_public_params(
 /// Proves the circuit, this function builds the public params and returns a recursive SNARK
 pub fn prove(
   circuit: &SigningCircuit<Fp>,
-  public_params: &PublicParams<E2>,
-) -> Result<RecursiveSNARK<E2>, Box<dyn error::Error>> {
-  type C2 = TrivialCircuit<<E1 as Engine>::Scalar>;
+  public_params: &PublicParams<E1>,
+) -> Result<RecursiveSNARK<E1>, Box<dyn error::Error>> {
+  type C2 = TrivialCircuit<<E2 as Engine>::Scalar>;
   let circuit_secondary = C2::default();
-  let z0_primary = [<E2 as Engine>::Scalar::ZERO];
-  let z0_secondary = [<E1 as Engine>::Scalar::ZERO];
+  let z0_primary = [<E1 as Engine>::Scalar::ZERO];
+  let z0_secondary = [<E2 as Engine>::Scalar::ZERO];
 
-  let mut recursive_snark: RecursiveSNARK<E2> = RecursiveSNARK::<E2>::new(
+  let mut recursive_snark: RecursiveSNARK<E1> = RecursiveSNARK::<E1>::new(
     &public_params,
     circuit,
     &circuit_secondary,
@@ -154,22 +181,22 @@ pub fn prove(
 
 /// Compressed a recursive SNARK into a compressed SNARK, using the circuit public params
 pub fn compress_proof(
-  public_params: &PublicParams<E2>,
-  recursive_snark: &RecursiveSNARK<E2>,
-) -> Result<CompressedSNARK<E2, S2, S1>, Box<dyn error::Error>> {
-  let (pk, _) = CompressedSNARK::<E2, S2, S1>::setup(&public_params)?;
+  public_params: &PublicParams<E1>,
+  recursive_snark: &RecursiveSNARK<E1>,
+) -> Result<CompressedSNARK<E1, S1, S2>, Box<dyn error::Error>> {
+  let (pk, _) = CompressedSNARK::<E1, S1, S2>::setup(&public_params)?;
   let compressed_proof =
-    CompressedSNARK::<E2, S2, S1>::prove(&public_params, &pk, recursive_snark)?;
+    CompressedSNARK::<E1, S1, S2>::prove(&public_params, &pk, recursive_snark)?;
   Ok(compressed_proof)
 }
 
 /// Verifies a recursive SNARK, returns the field elements of the circuit output
 pub fn verify(
-  public_params: &PublicParams<E2>,
-  recursive_snark: &RecursiveSNARK<E2>,
-) -> Result<Vec<<E2 as Engine>::Scalar>, Box<dyn error::Error>> {
-  let z0_primary = [<E2 as Engine>::Scalar::ZERO];
-  let z0_secondary = [<E1 as Engine>::Scalar::ZERO];
+  public_params: &PublicParams<E1>,
+  recursive_snark: &RecursiveSNARK<E1>,
+) -> Result<Vec<<E1 as Engine>::Scalar>, Box<dyn error::Error>> {
+  let z0_primary = [<E1 as Engine>::Scalar::ZERO];
+  let z0_secondary = [<E2 as Engine>::Scalar::ZERO];
 
   let res = recursive_snark.verify(&public_params, 1, &z0_primary, &z0_secondary);
   let (vec, _) = res?;
@@ -179,13 +206,13 @@ pub fn verify(
 
 /// Verifies a compressed SNARK, returns the field elements of the circuit output
 pub fn verify_compressed(
-  public_params: &PublicParams<E2>,
-  compressed_proof: &CompressedSNARK<E2, S2, S1>,
-) -> Result<Vec<<E2 as Engine>::Scalar>, Box<dyn error::Error>> {
-  let z0_primary = [<E2 as Engine>::Scalar::ZERO];
-  let z0_secondary = [<E1 as Engine>::Scalar::ZERO];
+  public_params: &PublicParams<E1>,
+  compressed_proof: &CompressedSNARK<E1, S1, S2>,
+) -> Result<Vec<<E1 as Engine>::Scalar>, Box<dyn error::Error>> {
+  let z0_primary = [<E1 as Engine>::Scalar::ZERO];
+  let z0_secondary = [<E2 as Engine>::Scalar::ZERO];
 
-  let (_, vk) = CompressedSNARK::<E2, S2, S1>::setup(&public_params)?;
+  let (_, vk) = CompressedSNARK::<E1, S1, S2>::setup(&public_params)?;
   let (res, _) = compressed_proof.verify(&vk, 1, &z0_primary, &z0_secondary)?;
 
   Ok(res)
